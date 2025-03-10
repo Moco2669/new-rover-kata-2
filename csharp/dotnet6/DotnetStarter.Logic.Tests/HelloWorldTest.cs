2294066b16b3c0da4e2bbd6e0435863ce37470b3
Moco2669 <moxtheboom004@gmail.com>
2025-03-10 18:03:55 +0100

    rover moves one point

--- csharp/dotnet6/DotnetStarter.Logic.Tests/HelloWorldTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/HelloWorldTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace DotnetStarter.Logic.Tests
@@ -6,5 +7,20 @@ namespace DotnetStarter.Logic.Tests
     {
         [Fact]
         public void Hello_ReturnsWorld() => Assert.Equal("World!", HelloWorld.Hello());
+
+        [Fact]
+        public void RoverMovesOnePoint()
+        {
+            Rover rover = new();
+            Assert.Equal("0:1:N", rover.Move());
+        }
+    }
+
+    public class Rover
+    {
+        public string Move()
+        {
+            return "0:1:N";
+        }
     }
 }
