e72129c7ab056a89385eb7322ed48c0d6abe9158
Moco2669 <moxtheboom004@gmail.com>
2025-03-17 20:56:55 +0100

    rover stops at obstacle

--- csharp/dotnet6/DotnetStarter.Logic.Tests/RoverHasObstaclesUseCase.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/RoverHasObstaclesUseCase.cs
@@ -14,4 +14,15 @@ public class RoverHasObstaclesUseCase
         string stepsToObstacle = "RMMLMM";
         Assert.Equal("O:2:2:N", rover.ExecuteInput(stepsToObstacle));
     }
+
+    [Fact]
+    public void RoverStopsAtObstacle()
+    {
+        int obstacleXPos = 2;
+        int obstacleYPos = 3;
+        Rover rover = new();
+        rover.AddObstacle(obstacleXPos, obstacleYPos);
+        string forceablyTryToMovePastObstacle = "RMMLMMMMMMM";
+        Assert.Equal("O:2:2:N", rover.ExecuteInput(forceablyTryToMovePastObstacle));
+    }
 }
